dc873923a02961d27c909e9c93cbdd02b66af442
RemiixInc <39117916+RemiixInc@users.noreply.github.com>
2020-12-09 11:24:38 +1100

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", function() {
+  console.log("Connected");
+});
+
+client.on("message", message => {
+  if (message.mentions.has("BOT ID")) {
+    (async () => {
+      var voiceChannel = message.member.voice.channel;
+      voiceChannel
+        .join()
+        .then(async connection => {
+          const dispatcher = connection.play(
+            "https://hi.com"
+          );
+          dispatcher.on("finish", end => {
+            voiceChannel.leave();
+          });
+        })
+        .catch(err => console.log(err));
+    })();
+  }
+});
+
+client.login("BOT TOKEN");
